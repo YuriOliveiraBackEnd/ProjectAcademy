e9c3a6827021a7a1c76d9d50451fbf2d0445ef78
Yuri <yurixxx37@gmail.com>
2023-03-29 03:12:34 -0300

    Tela IMC secund version, responsive

--- FrontEnd/TelaIMC/calcIMC.js
+++ FrontEnd/TelaIMC/calcIMC.js
@@ -9,19 +9,9 @@ function calculaIMC(){
         document.getElementById("altura").value="";
         document.getElementById("peso").value="";
     }
-    else if(imc <18.5){
-        document.getElementById("resultado").value="Magreza " +imc;
+    else{
+        const total = imc.toFixed(1);
+        document.getElementById("resultado").value= total +"   "+"Agora confira a tabela abaixo";
     }
-    else if(imc <25){
-        document.getElementById("resultado").value="Normal " +imc;
-    }
-    else if(imc <30){
-        document.getElementById("resultado").value="Sobrepeso " +imc;
-    }
-    else if(imc <40){
-        document.getElementById("resultado").value="Obesidade " +imc;
-    }
-    else if(imc =40){
-        document.getElementById("resultado").value="Obesidade grave " +imc;
-    }
+  
 }
