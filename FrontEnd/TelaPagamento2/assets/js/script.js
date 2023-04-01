497c68fd4d6fbd25f8cb5c25512a691e947da665
Lucas <lc3197@gmail.com>
2023-04-01 18:50:48 -0300

    fifth version, tela pagamento

---
+++ FrontEnd/TelaPagamento2/assets/js/script.js
@@ -0,0 +1,7 @@
+ // menu no mobile->
+ const butmobile = document.getElementById('button_mob');
+function Menutoggle(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+butmobile.addEventListener("click", Menutoggle);
